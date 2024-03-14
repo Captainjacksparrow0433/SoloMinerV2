e708b211c4aa8a2045f8f1eaaf0b33d7d4be24c9
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2024-03-14 20:00:00 +0530

    Update SoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = '1LGXUpYQRdNQVVaZCmArJpm72NE94hrJkq' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
